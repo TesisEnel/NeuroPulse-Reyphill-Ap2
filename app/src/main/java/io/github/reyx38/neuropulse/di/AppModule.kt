a31a1027546c0a44dac7de03eca0df723a165172
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-26 10:46:02 -0400

    feat(Module) add API module and APP module for managing data local and remote repositories

---
+++ app/src/main/java/io/github/reyx38/neuropulse/di/AppModule.kt
@@ -0,0 +1,10 @@
+package io.github.reyx38.neuropulse.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+}
