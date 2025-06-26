e82f95cd5fd3ef10e4a05bdfe15311b3623370b6
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-26 12:06:54 -0400

    feat(Navigation): add NavHost for managing all composables and navigation of app

---
+++ app/src/main/java/io/github/reyx38/neuropulse/presentation/navigation/NeuroPulseNavHost.kt
@@ -0,0 +1,24 @@
+package io.github.reyx38.neuropulse.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+
+
+@Composable
+fun NeuroPulseNavHost (
+    navHostController: NavHostController,
+){
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.Home
+    ) {
+        composable<Screen.Home> {
+
+        }
+    }
+
+}
+
