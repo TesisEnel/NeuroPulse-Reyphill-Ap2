918e3b6b671f4a27c6b980004fa6cb78fffe9e60
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-26 12:05:49 -0400

    feat(Navigation): add sealed class of screen

---
+++ app/src/main/java/io/github/reyx38/neuropulse/presentation/navigation/Screen.kt
@@ -0,0 +1,8 @@
+package io.github.reyx38.neuropulse.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+    @Serializable
+    data object Home: Screen()
+}
