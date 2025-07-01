15ac3159bd48dcc94fb8099ae74f651bee0e0c84
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-01 16:09:57 -0400

    feat(usuario): add UsuarioDao to database

--- app/src/main/java/io/github/reyx38/neuropulse/data/local/database/Database.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/local/database/Database.kt
@@ -3,6 +3,7 @@ package io.github.reyx38.neuropulse.data.local.database
 import io.github.reyx38.neuropulse.data.local.entities.UserEntity
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import io.github.reyx38.neuropulse.data.local.dao.UsuarioDao
 
 @Database(
     entities = [
@@ -12,5 +13,5 @@ import androidx.room.RoomDatabase
     exportSchema = false
 )
 abstract class NeuroPulseDb : RoomDatabase() {
-
+    abstract fun usuarioDao() : UsuarioDao
 }
