8d7d8da91edf4b76766c116bc31aaa738602be17
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-01 16:10:40 -0400

    feat(usuario): add module of database and dao to AppModule

--- app/src/main/java/io/github/reyx38/neuropulse/di/AppModule.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/di/AppModule.kt
@@ -1,10 +1,27 @@
 package io.github.reyx38.neuropulse.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.github.reyx38.neuropulse.data.local.database.NeuroPulseDb
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class AppModule {
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNeuroPulseBd(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            NeuroPulseDb::class.java,
+            "NeuroPulse.db"
+        ).fallbackToDestructiveMigration().build()
+
+    @Provides
+    fun provideUsuarioDao(neuroPulseDb: NeuroPulseDb) = neuroPulseDb.usuarioDao()
 }
