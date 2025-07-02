c28e02ccc143342e2ef010846db8a51412d9c181
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-01 23:06:30 -0400

    feat(NeuroPulseApi): add api module

--- app/src/main/java/io/github/reyx38/neuropulse/di/ApiModule.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/di/ApiModule.kt
@@ -1,9 +1,44 @@
 package io.github.reyx38.neuropulse.di
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.github.reyx38.neuropulse.data.remote.NeuroPulseApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class ApiModule {
+object ApiModule {
+    const val BASE_URL = "https://neuropulseapi.azurewebsites.net/"
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesNeuroPulseApi(moshi: Moshi): NeuroPulseApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(NeuroPulseApi::class.java)
+    }
 }
