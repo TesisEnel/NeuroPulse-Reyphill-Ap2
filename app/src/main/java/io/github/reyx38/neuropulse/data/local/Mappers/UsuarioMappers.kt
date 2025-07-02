8309b605ac7fb1c13f12434b7452560baf79643f
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-01 23:05:28 -0400

    feat(usuario): add map to user entity

---
+++ app/src/main/java/io/github/reyx38/neuropulse/data/local/Mappers/UsuarioMappers.kt
@@ -0,0 +1,15 @@
+package io.github.reyx38.neuropulse.data.local.Mappers
+
+import io.github.reyx38.neuropulse.data.local.entities.UserEntity
+import io.github.reyx38.neuropulse.data.remote.dto.UsuarioDto
+
+fun UsuarioDto.toEntity(): UserEntity {
+    return UserEntity(
+        usuarioId = usuarioId,
+        nombreUsuario = nombre,
+        telefono = telefono,
+        email = email,
+        token = token,
+        imagenPerfil = null
+    )
+}
