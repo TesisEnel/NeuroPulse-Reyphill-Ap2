f0f641e889e0a0ec3316c8d5ac50062f9ef853f2
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-01 22:59:02 -0400

    feat(login): add state and event for the login

---
+++ app/src/main/java/io/github/reyx38/neuropulse/presentation/login/LoginUiEvent.kt
@@ -0,0 +1,9 @@
+package io.github.reyx38.neuropulse.presentation.login
+
+sealed interface LoginUiEvent {
+    data class NombreChange(val nombre: String): LoginUiEvent
+    data class PasswordChange(val password: String): LoginUiEvent
+
+    data object Login: LoginUiEvent
+    data object New: LoginUiEvent
+}
---
+++ app/src/main/java/io/github/reyx38/neuropulse/presentation/login/LoginUiState.kt
@@ -0,0 +1,13 @@
+package io.github.reyx38.neuropulse.presentation.login
+
+import io.github.reyx38.neuropulse.data.local.entities.UserEntity
+
+data class LoginUiState (
+    val nombre: String = "",
+    val errorNombre: String? = null,
+    val password: String = "",
+    val errorPassword: String? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val user: UserEntity? = null
+)
