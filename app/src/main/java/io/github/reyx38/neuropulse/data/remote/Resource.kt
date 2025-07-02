9f2478ce446b07b9d5a4e524aebb4bd64a847738
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-01 23:02:07 -0400

    add(Resource): implementation Resource

--- app/src/main/java/io/github/reyx38/neuropulse/data/remote/Resource.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/remote/Resource.kt
@@ -1,4 +1,13 @@
 package io.github.reyx38.neuropulse.data.remote
 
-class Resource {
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String?, data: T? = null) : Resource<T>(data, message)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+
+    fun getErrorMessage(): String? = when (this) {
+        is Error -> message
+        else -> null
+    }
+
 }
