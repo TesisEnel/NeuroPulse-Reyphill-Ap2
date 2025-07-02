9bf2ae502e9450989b671a975395a2b9ce3f36e7
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-01 23:07:21 -0400

    feat(NeuroPulseApi): add Api methods

--- app/src/main/java/io/github/reyx38/neuropulse/data/remote/NeuroPulseApi.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/remote/NeuroPulseApi.kt
@@ -1,4 +1,15 @@
 package io.github.reyx38.neuropulse.data.remote
 
-class NeuroPulseApi {
+import io.github.reyx38.neuropulse.data.remote.dto.PeticionLogin
+import io.github.reyx38.neuropulse.data.remote.dto.UsuarioDto
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface NeuroPulseApi {
+    //Usuarios/Registrar
+    @POST("api/Usuarios/register")
+    suspend fun registrarUsuario(@Body usuarioDto: UsuarioDto)
+    //Usuario/Login
+    @POST("api/Usuarios/login")
+    suspend fun loginUsuario(@Body peticionLogin: PeticionLogin ): UsuarioDto
 }
