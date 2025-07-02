114bd0a2f692fece5b71868e8caea191f4b7f5c7
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-01 23:07:02 -0400

    feat(NeuroPulseApi): add Api methods

--- app/src/main/java/io/github/reyx38/neuropulse/data/remote/RemoteDataSource.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/remote/RemoteDataSource.kt
@@ -1,4 +1,12 @@
 package io.github.reyx38.neuropulse.data.remote
 
-class RemoteDataSource {
+import io.github.reyx38.neuropulse.data.remote.dto.PeticionLogin
+import io.github.reyx38.neuropulse.data.remote.dto.UsuarioDto
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val neuroPulseApi: NeuroPulseApi
+) {
+    suspend fun registerUsuario(usuarioDto: UsuarioDto) = neuroPulseApi.registrarUsuario(usuarioDto)
+    suspend fun loginUsuario(peticionLogin: PeticionLogin) = neuroPulseApi.loginUsuario(peticionLogin)
 }
