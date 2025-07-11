8e22bc758e6dbe8476fe2d226e3ef5cc1a4b6b90
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-11 11:09:46 -0400

    feat(Api): add new methods for managment the updates of users

--- app/src/main/java/io/github/reyx38/neuropulse/data/remote/NeuroPulseApi.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/remote/NeuroPulseApi.kt
@@ -2,14 +2,22 @@ package io.github.reyx38.neuropulse.data.remote
 
 import io.github.reyx38.neuropulse.data.remote.dto.PeticionLogin
 import io.github.reyx38.neuropulse.data.remote.dto.UsuarioDto
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface NeuroPulseApi {
     //Usuarios/Registrar
     @POST("api/Usuarios/register")
     suspend fun registrarUsuario(@Body usuarioDto: UsuarioDto)
+
     //Usuario/Login
     @POST("api/Usuarios/login")
-    suspend fun loginUsuario(@Body peticionLogin: PeticionLogin ): UsuarioDto
+    suspend fun loginUsuario(@Body peticionLogin: PeticionLogin): UsuarioDto
+
+    //Usario/Edit
+    @PUT("api/Usuarios/{id}")
+    suspend fun updateUsuario(@Path("id") id: Int, @Body usuarioDto: UsuarioDto) : Response<Unit>
 }
