87ba6365ccdc2340cd4ce970af92e62628ce8f00
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-20 04:36:36 -0400

    feat(reflexion): add save and list to remotedatasource

--- app/src/main/java/io/github/reyx38/neuropulse/data/remote/RemoteDataSource.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/remote/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package io.github.reyx38.neuropulse.data.remote
 
 import io.github.reyx38.neuropulse.data.remote.dto.PeticionLogin
+import io.github.reyx38.neuropulse.data.remote.dto.ReflexionDto
 import io.github.reyx38.neuropulse.data.remote.dto.UsuarioDto
 import javax.inject.Inject
 
@@ -10,4 +11,7 @@ class RemoteDataSource @Inject constructor(
     suspend fun registerUsuario(usuarioDto: UsuarioDto) = neuroPulseApi.registrarUsuario(usuarioDto)
     suspend fun loginUsuario(peticionLogin: PeticionLogin) = neuroPulseApi.loginUsuario(peticionLogin)
     suspend fun updateUsuario(id: Int, usuarioDto: UsuarioDto) = neuroPulseApi.updateUsuario(id, usuarioDto)
+    //Reflexiones
+    suspend fun getReflexiones(usuarioId: Int) = neuroPulseApi.getReflexiones(usuarioId)
+    suspend fun saveReflexion(reflexionDto: ReflexionDto) = neuroPulseApi.saveReflexion(reflexionDto)
 }
