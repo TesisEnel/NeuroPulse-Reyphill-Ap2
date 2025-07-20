8baf41b4c6c7914a16c58d0cf55df360251fffc5
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-20 04:29:57 -0400

    feat(database): add new entity and new schema

--- app/src/main/java/io/github/reyx38/neuropulse/data/local/database/Database.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/local/database/Database.kt
@@ -3,15 +3,22 @@ package io.github.reyx38.neuropulse.data.local.database
 import io.github.reyx38.neuropulse.data.local.entities.UserEntity
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import io.github.reyx38.neuropulse.data.local.dao.ReflexionDao
 import io.github.reyx38.neuropulse.data.local.dao.UsuarioDao
+import io.github.reyx38.neuropulse.data.local.entities.ReflexionEntity
 
 @Database(
     entities = [
-        UserEntity::class
+        UserEntity::class,
+        ReflexionEntity::class
     ],
-    version = 2,
+    version = 6,
     exportSchema = false
 )
+
+@TypeConverters(Converters::class)
 abstract class NeuroPulseDb : RoomDatabase() {
     abstract fun usuarioDao() : UsuarioDao
+    abstract fun reflexionDao(): ReflexionDao
 }
