b7289f8c0273751e3b01c5bf0055428842ad3634
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-20 04:32:23 -0400

    feat(Reflexion): add Reflexion repositorys

---
+++ app/src/main/java/io/github/reyx38/neuropulse/data/repository/ReflexionRepository.kt
@@ -0,0 +1,54 @@
+package io.github.reyx38.neuropulse.data.repository
+
+import io.github.reyx38.neuropulse.data.local.Mappers.toDto
+import io.github.reyx38.neuropulse.data.local.Mappers.toEntity
+
+import io.github.reyx38.neuropulse.data.local.dao.ReflexionDao
+import io.github.reyx38.neuropulse.data.remote.RemoteDataSource
+import io.github.reyx38.neuropulse.data.remote.Resource
+import io.github.reyx38.neuropulse.data.remote.dto.ReflexionDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ReflexionRepository@Inject constructor(
+    private val remoteDataSource: RemoteDataSource,
+    private val reflexionDao: ReflexionDao
+) {
+    fun getReflexiones(usuarioId: Int): Flow<Resource<List<ReflexionDto>>>{
+        return flow {
+            emit(Resource.Loading())
+            val localFlow = reflexionDao.listarReflexion(usuarioId)
+                .map { list ->
+                    Resource.Success(list.map { it.toDto() })
+                }
+            try  {
+               val remote = remoteDataSource.getReflexiones(usuarioId)
+                if(remote.isNotEmpty())
+                    reflexionDao.deleteAllReflexion(usuarioId)
+                remote.forEach { reflexionDao.save(it.toEntity()) }
+            }catch (e: Exception){
+              emit(Resource.Error("Error: ${e.localizedMessage ?: "Error desconocido"}"))
+            }
+            emitAll(localFlow)
+
+        }
+
+    }
+
+    suspend fun save (reflexionDto: ReflexionDto): Resource<Unit>{
+        return try {
+
+            remoteDataSource.saveReflexion(reflexionDto)
+
+            Resource.Success(Unit)
+        }catch (e: Exception){
+            Resource.Error("Huubo un error al guardar los datos: ${e.localizedMessage}")
+        }
+    }
+
+}
