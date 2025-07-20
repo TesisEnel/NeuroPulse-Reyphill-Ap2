9a6afa1f5f65fab72ba76bce97b92396e2b357b8
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-20 04:34:19 -0400

    feat(reflexion): add methods to save and list reflexion

--- app/src/main/java/io/github/reyx38/neuropulse/data/remote/NeuroPulseApi.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/remote/NeuroPulseApi.kt
@@ -1,9 +1,11 @@
 package io.github.reyx38.neuropulse.data.remote
 
 import io.github.reyx38.neuropulse.data.remote.dto.PeticionLogin
+import io.github.reyx38.neuropulse.data.remote.dto.ReflexionDto
 import io.github.reyx38.neuropulse.data.remote.dto.UsuarioDto
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -20,4 +22,11 @@ interface NeuroPulseApi {
     //Usario/Edit
     @PUT("api/Usuarios/{id}")
     suspend fun updateUsuario(@Path("id") id: Int, @Body usuarioDto: UsuarioDto) : Response<Unit>
+
+    //Reflexiones/List
+    @GET("api/Reflexiones/usuario/{usuarioId}")
+    suspend fun getReflexiones(@Path("usuarioId") usuarioId: Int) : List<ReflexionDto>
+    //Reflexiones/Agregar
+    @POST("api/Reflexiones")
+    suspend fun saveReflexion(@Body reflexionDto: ReflexionDto): Response<Unit>
 }
