c8e988a4c185993ca60e7add978270bc5d695dee
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-24 17:32:14 -0400

    feat(InformacionRespiracion): add InformacionRespiracion DAO

---
+++ app/src/main/java/io/github/reyx38/neuropulse/data/remote/dto/InformacionRespiracionesDto.kt
@@ -0,0 +1,8 @@
+package io.github.reyx38.neuropulse.data.remote.dto
+
+data class InformacionRespiracionesDto(
+    val idInformacionRespiracion: Int,
+    val idRespiracion: Int,
+    val descripcion: String,
+    val tipoInformacion: String
+)
