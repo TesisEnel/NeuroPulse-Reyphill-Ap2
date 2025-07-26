ff57321564b9664795d94b3297b491bcf99f8d10
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-26 00:33:01 -0400

    refactor(reflexion): modify fecha on dto

--- app/src/main/java/io/github/reyx38/neuropulse/data/local/Mappers/ReflexionMappers.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/local/Mappers/ReflexionMappers.kt
@@ -2,45 +2,24 @@ package io.github.reyx38.neuropulse.data.local.Mappers
 
 import io.github.reyx38.neuropulse.data.local.entities.ReflexionEntity
 import io.github.reyx38.neuropulse.data.remote.dto.ReflexionDto
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
-
-fun ReflexionEntity.toDto(): ReflexionDto{
+fun ReflexionEntity.toDto(): ReflexionDto {
     return ReflexionDto(
         reflexionId,
         usuarioId,
         descripcion,
         estadoReflexion,
-        fechaCreacion = fechaCreacion.toDateStringOrNull()
+        fechaCreacion
     )
 }
+
 fun ReflexionDto.toEntity(): ReflexionEntity {
     return ReflexionEntity(
         reflexionId,
         usuarioId,
         descripcion,
         estadoReflexion,
-        fechaCreacion = fechaCreacion.toDateOrNull()
+        fechaCreacion
     )
 }
 
-fun Date?.toDateStringOrNull(): String? {
-    return try {
-        this?.let {
-            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US).format(it)
-        }
-    } catch (e: Exception) {
-        null
-    }
-}
-
-fun String?.toDateOrNull(): Date? {
-    return try {
-        if (this == null || this == "0001-01-01T00:00:00") null
-        else SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US).parse(this)
-    } catch (e: Exception) {
-        null
-    }
-}
