43a74ccb29ceb2298f36627edbbad2852a3a0ede
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-25 21:42:29 -0400

    feat(usuarios): add event to chage data of uistate (imagen)

--- app/src/main/java/io/github/reyx38/neuropulse/presentation/usuarios/perifilUsuarios/UsuarioEvent.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/presentation/usuarios/perifilUsuarios/UsuarioEvent.kt
@@ -5,6 +5,8 @@ sealed interface UsuarioEvent {
     data class NombreChange(val nombre: String ): UsuarioEvent
     data class EmailChange(val email: String) : UsuarioEvent
     data class TelefonoChange(val telefono: String) : UsuarioEvent
+    data class ImagenChange(val imagen: String) : UsuarioEvent
+
 
     data object Save: UsuarioEvent
     data object Delete: UsuarioEvent
