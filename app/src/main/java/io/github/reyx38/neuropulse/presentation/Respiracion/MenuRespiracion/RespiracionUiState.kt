2ff6284c7a224cf6ac8b84e4acd8acb30820af65
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-25 21:26:47 -0400

    refactor(SesionRespiracion): add methods on RespiracionviewModel and members in RespiracionUiState to use RespiracionScreen.kt

--- app/src/main/java/io/github/reyx38/neuropulse/presentation/Respiracion/MenuRespiracion/RespiracionUiState.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/presentation/Respiracion/MenuRespiracion/RespiracionUiState.kt
@@ -1,11 +1,25 @@
 package io.github.reyx38.neuropulse.presentation.Respiracion.MenuRespiracion
 
 import io.github.reyx38.neuropulse.data.local.entities.RespiracionWithInformacion
+import io.github.reyx38.neuropulse.data.local.entities.UserEntity
+import io.github.reyx38.neuropulse.data.remote.dto.SesionRespiracionDto
 
 data class RespiracionUiState(
-
+    val respiracionId: Int? = null,
+    var duracionMinutos: Int = 5,
+    val usuarioId: Int? = null,
+    val user: UserEntity? = null,
+    val estado: String? = null, // Completado, pausado, interrupido
     val isLoading: Boolean = false,
     val respiraciones: List<RespiracionWithInformacion> = emptyList(),
     val respiracion: RespiracionWithInformacion? = null,
     val error: String? = null
+)
+
+fun RespiracionUiState.toDto() = SesionRespiracionDto(
+    respiracionid =  respiracionId,
+    usuarioId = usuarioId,
+    estado = estado,
+    duracionMinutos = duracionMinutos,
+    sesionId = 0
 )
