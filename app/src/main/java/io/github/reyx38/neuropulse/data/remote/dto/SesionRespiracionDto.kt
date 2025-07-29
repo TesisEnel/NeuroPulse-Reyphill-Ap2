48bf663ab0838fcfe23963b2fff84ed46e3af20a
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-29 11:59:19 -0400

    refactor(SesionRespiracion): modify dto for send the correct data to Api

--- app/src/main/java/io/github/reyx38/neuropulse/data/remote/dto/SesionRespiracionDto.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/data/remote/dto/SesionRespiracionDto.kt
@@ -1,10 +1,11 @@
 package io.github.reyx38.neuropulse.data.remote.dto
 
 data class SesionRespiracionDto (
-    val sesionId: Int?,
-    val respiracionid: Int?,
-    val usuarioId: Int?,
-    val duracionMinutos: Int?,
-    val estado: String?
+    val idSesionRespiracion: Int,
+    val idRespiracion: Int,
+    val idUsuario: Int,
+    val duracionMinutos: Int,
+    val estado: String,
+    val fechaRealizada: String?
 
 )
