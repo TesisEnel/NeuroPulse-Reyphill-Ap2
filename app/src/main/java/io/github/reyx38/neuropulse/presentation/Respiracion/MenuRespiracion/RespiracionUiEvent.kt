ba53b069a5ec77221d733a63d15d07e68beecdf6
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-07-29 12:06:09 -0400

    refactor(SesionRespiracion): add methods of uiEvent for management the member state

--- app/src/main/java/io/github/reyx38/neuropulse/presentation/Respiracion/MenuRespiracion/RespiracionUiEvent.kt
+++ app/src/main/java/io/github/reyx38/neuropulse/presentation/Respiracion/MenuRespiracion/RespiracionUiEvent.kt
@@ -6,6 +6,7 @@ sealed interface RespiracionUiEvent {
     data class RespiracionChange (val respiracionId: Int) : RespiracionUiEvent
     data class DuracionMinutos (val minutos: Int) : RespiracionUiEvent
     data class UsuarioChange (val usuarioId: Int) : RespiracionUiEvent
+    data class EstadoChange(val estado: String): RespiracionUiEvent
 
     object Save: RespiracionUiEvent
     object New: RespiracionUiEvent
